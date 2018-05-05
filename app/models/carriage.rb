477c6c9bacd550de37f4ea47117fbbc6e1fbc111
Achishka <ikona@mail.ua>
2018-05-06 00:10:37 +0300

    Added scopes for Carriage

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -2,4 +2,9 @@ class Carriage < ApplicationRecord
   belongs_to :train
 
   validates :number, presence: true
+
+  scope :eco, -> {where(type: 'EcoCarriage')}
+  scope :coupe, -> {where(type: 'CoupeCarriage')}
+  scope :sleep, -> {where(type: 'SleepCarriage')}
+  scope :chair, -> {where(type: 'ChairCarriage')}
 end
